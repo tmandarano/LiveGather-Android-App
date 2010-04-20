801870697adc0830b6ec3222a6cc2a8776d72c79
Preston Crawford <preston.crawford@gmail.com>
2010-04-20 01:26:21 -0700

    Initial commit

---
+++ src/com/livegather/android/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.livegather.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateAccountActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/livegather/android/LiveGatherApplication.java
@@ -0,0 +1,7 @@
+package com.livegather.android;
+
+import android.app.Application;
+
+public class LiveGatherApplication extends Application {
+
+}
