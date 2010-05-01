eea54de8aaba7b2396b52c84ac4b0755acc8f489
Preston Crawford <preston.crawford@gmail.com>
2010-04-30 18:07:04 -0700

    Pushign new androind changes

--- src/com/livegather/android/LiveGatherApplication.java
+++ src/com/livegather/android/LiveGatherApplication.java
@@ -1,7 +1,22 @@
 package com.livegather.android;
 
+import com.livegather.android.model.User;
+
 import android.app.Application;
 
 public class LiveGatherApplication extends Application {
+	private User user;
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public String getUrl() {
+		return "http://192.168.1.111";
+	}
 
 }
